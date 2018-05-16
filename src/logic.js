7feebccf1a27dbc9490be044e0b12a282f04c7b6
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-16 13:11:29 +0200

    First commit

---
+++ src/logic.js
@@ -0,0 +1,12 @@
+import { allocations } from './stage'
+
+export const logic = ($message) => {
+    return new Promise((resolve, reject) => {
+        const response = stage[Math.floor(Math.random() * stage.length)];
+    const newMessage = Object.assign($message.message, { content: response })
+    const newIntentMessage = Object.assign($message, { message: newMessage })
+    resolve(newIntentMessage)
+    })
+}
+
+
---
+++ src/stage.js
@@ -0,0 +1,5 @@
+export const stage = [
+    "Pour plus d'informations sur les conventions de stage, rendez-vous sur https://www.pole-emploi.fr/actualites/la-convention-de-stage-@/article.jspz?id=74795",
+    "Pôle-Emploi ne propose malheureusement pas d'offres de stage actuellement :("
+
+]
