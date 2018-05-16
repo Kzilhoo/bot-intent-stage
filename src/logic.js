be08bf764d54b4c44b0b4af714256aba6a0138ae
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-16 18:02:31 +0200

    Add .env to gitignore, changes to rabbitConnexion.js, logic.js, rabbitUtil.js

--- src/logic.js
+++ src/logic.js
@@ -4,9 +4,11 @@ export const logic = ($message) => {
     return new Promise((resolve, reject) => {
         const response = stage[Math.floor(Math.random() * stage.length)];
     const newMessage = Object.assign($message.message, { content: response })
-    const newIntentMessage = Object.assign($message, { message: newMessage })
-    resolve(newIntentMessage)
+
+    resolve(newMessage)
     })
 }
+
+
 
 
